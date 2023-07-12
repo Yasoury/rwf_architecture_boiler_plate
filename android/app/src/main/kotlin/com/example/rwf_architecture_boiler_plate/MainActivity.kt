aa7e74078cc3c000888591c8c8478a071dbcb7d9
Mohammed Ali <malyasoury@gmail.com>
2023-07-12 22:28:55 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/rwf_architecture_boiler_plate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rwf_architecture_boiler_plate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RWF Architecture',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Welcome to the RWF arch"),
+    );
+  }
+}
