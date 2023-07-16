a3ac79d9d924c29e79b543bd96190288f3ac3e3f
Mohammed Al-Yasory <88273182+Yasoury@users.noreply.github.com>
2023-07-16 16:12:15 +0300

    added localization

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,12 @@
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get quotesBottomNavigationBarItemLabel => 'الصفحة الرئيسية';
+
+  @override
+  String get profileBottomNavigationBarItemLabel => 'الملف الشخصي';
+}
